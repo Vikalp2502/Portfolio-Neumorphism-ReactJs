233f307113c2f5c918a6ab779df4b633bde24620
Vikalp Kaushik <62279288+Vikalp2502@users.noreply.github.com>
2020-09-15 13:15:30 +0530

    Update Contact.js
    some changes

--- src/contents/Contact.js
+++ src/contents/Contact.js
@@ -7,11 +7,11 @@ class Contact extends Component {
         return (
             <div className="condiv contact">
             <h1 className="subtopic">Contact Me</h1>
-            <div class="port_out con_out"><div class="con_in"><img src={Map} class="co1"></img></div></div>
+            <div className="hr_line line2"><div className="inner2 inner3"></div></div>
+            <div class="con_out"><div class="con_in"><img src={Map} class="co1"></img></div></div>
             <a href="#"><div className="back3">Download Resume</div></a>
             <a href="mailto:youremail@gmail.com"><div className="back3 back4">Contact Me</div></a>
-            <div className="hr_line line2"><div className="inner2 inner3"></div></div>
-            <div className="hr_line line3"><div className="inner2 inner3"></div></div>
+            <div className="hr_line line3"><div className="inner3"></div></div>
             <Social />
             </div>
             )
